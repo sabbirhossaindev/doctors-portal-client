b8aafdaafafe94fa4fd9a6ee411a16dd8dd043f1
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-13 01:19:25 +0600

    login

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,10 +1,30 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from './AppointmentOption';
 
-const AvailableAppointment = ({selectedDate}) => {
+const AvailableAppointment = ({ selectedDate }) => {
+    
+    const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
+
+    useEffect(() => {
+        fetch('appointmentOptions.json')
+            .then(res => res.json())
+            .then(data => setAppointmentOptions(data))
+    }, [])
+
     return (
-        <div>
+        <div className='my-16'>
             <h3 className='font-bold text-center text-xl text-secondary'>Available Appointments on {format(selectedDate, 'pp')}</h3>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
+                    appointmentOptions.map(option => <AppointmentOption
+                        key={option._id}
+                        appointmentOption={option}
+                        setTreatment={setTreatment}
+                    ></AppointmentOption>)
+                }
+            </div>
         </div>
     );
 };
