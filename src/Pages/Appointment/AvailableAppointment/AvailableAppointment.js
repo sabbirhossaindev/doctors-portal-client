1424c0eed8487f844a92234375de441e9f276792
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-13 23:01:51 +0600

    login

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -16,9 +16,10 @@ const AvailableAppointment = ({ selectedDate }) => {
 
     return (
         <div className='my-16'>
-            <h3 className='font-bold text-center text-xl text-secondary'>Available Appointments on {format(selectedDate, 'PP')}</h3>
+            <h3 className='font-bold text-center text-xl text-secondary'>Available Appointments on Date    :- {format(selectedDate, 'PP')}</h3>
 
-            <h3 className='font-bold text-center text-xl text-secondary'>Available Appointments on {format(selectedDate, 'pp')}</h3>
+            <h3 className='font-bold text-center text-xl text-secondary'> viewer Time On  :- {format(selectedDate, 'pp')}</h3>
+            
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
                 {
                     appointmentOptions.map(option => <AppointmentOption
