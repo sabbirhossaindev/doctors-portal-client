2ff79e877ca70760f08f607bae7beb173f0a4274
sabbir hossain <mdsabbir477470@gmail.com>
2023-06-08 21:27:55 +0600

    contact done

--- src/Pages/Home/ContactUs/ContactUs.js
+++ src/Pages/Home/ContactUs/ContactUs.js
@@ -1,48 +1,39 @@
 import React from 'react';
 import appointment from '../../../assets/images/appointment.png';
-import { Link } from 'react-router-dom';
+import './Contact.css';
 
 const ContactUs = () => {
     return (
-        <section
-        style={{
+        <section className="hero mt-16" style={{
             background: `url(${appointment})`,
             backgroundSize: 'cover'
-            }}
-        className='p-10 rounded-lg' data-aos="flip-left"
-        data-aos-easing="ease-out-cubic"
-        data-aos-duration="2000">
+            }}>
+
             <div className='text-center'>
                 <h3 className='font-bold text-xl text-primary'>Contact Us</h3>
                 <h1 className='text-white font-bold text-4xl'>Stay connected with us</h1>
-            </div>
-            
-            <div className="hero my-10">
-                <div className="hero-content flex-col lg:flex-row">
-                <div className='w-full' data-aos="fade-left" data-aos-anchor="#example-anchor" data-aos-offset="500" data-aos-duration="500">
-                            <h1 className="text-white font-bold text-4xl">Location </h1>
-                            <div className='px-10 py-10'>
-                        <p><iframe src="https://goo.gl/maps/TqvxeEV6i8jiNr2P8" width="350px" height="350" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>
-                        </div>
-                        </div>
-                            <div className=' w-full' data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-                            <div className="card-body">
-                            <div className="form-control">
-                                <input type="text" placeholder="Email Address" className="input input-bordered" />
+            <div className="hero-content flex-col lg:flex-row gap-16">
+                <div className='rounded-xl lg:w-1/2'data-aos="flip-left">
+                    <iframe className='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29073.83467664921!2d91.38732527780857!3d24.373324226682062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375144f6464c0833%3A0xb6a116d243e411d2!2z4KaG4Kan4KeB4Kao4Ka_4KaVIOCmnOCnh-CmsuCmviDgprjgpqbgprAg4Ka54Ka-4Ka44Kaq4Ka-4Kak4Ka-4KayLCDgprngpqzgpr_gppfgpp7gp43gppw!5e0!3m2!1sen!2sbd!4v1686234041758!5m2!1sen!2sbd" width="300" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div data-aos="fade-down-left">
+                    <div className="card-body w-full">
+                                <div className="form-control">
+                                    <input type="text" placeholder="Email Address" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <input type="text" placeholder="Subject" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <textarea className="textarea textarea-accent" placeholder="Your message"></textarea>
+                                </div>
                             </div>
-                            <div className="form-control">
-                                <input type="text" placeholder="Subject" className="input input-bordered" />
+                            <div className='mb-8 text-center'>
+                                    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Submit</button>
                             </div>
-                            <div className="form-control">
-                                <textarea className="textarea textarea-accent" placeholder="Your message"></textarea>
-                            </div>
-                        </div>
-                        <div className='mb-8 text-center'>
-                                <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Submit</button>
-                        </div>
-                        </div>
                     </div>
                 </div>
+            </div>
         </section>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,6 +3,15 @@ import { Link } from 'react-router-dom';
 import footer from '../../../assets/images/footer.png';
 
 const Footer = () => {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // Months start at 0!
+    let dd = today.getDate();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+
+    const formattedToday = dd + '/' + mm + '/' + yyyy;
     return (
         <footer
             style={{
@@ -33,7 +42,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className='text-center mt-32'>
-                <p className='text-xl font-bold text-secondary'>Copyright © 2022 - All right reserved by Doctor's Portal Ltd.</p>
+                <p className='text-xl font-bold text-secondary'>Copyright {formattedToday} - All right reserved by ©  Doctor's Portal Ltd {new Date().getFullYear()}.</p>
             </div>
         </footer>
     );
